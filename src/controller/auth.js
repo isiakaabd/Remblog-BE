4ae3f47117370165f7b2fc928634ffc4b98bee52
ISIAKA ABDULAHI <56041462+isiakaabd@users.noreply.github.com>
2023-08-14 17:48:29 +0100

    added to fix cross platform cookies

--- src/controller/auth.js
+++ src/controller/auth.js
@@ -22,7 +22,7 @@ const login = async (req, res) => {
     .cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
     })
     .status(StatusCodes.OK)
     .json({ success: true, user: { username: user.username } });
